6ff47946524a29351081cc761767297e2030fc1c
MeronKHZ <meronkba@gmail.com>
2025-12-09 09:41:36 +0100

    Revert "Includes player shooting, AI patrol, NavMesh updates, key/door system, particles, lighting changes, and final scene setup."
    This reverts commit 9665c29ef57a0943a2bbdf59b8c66cd46a43a41e.

--- Pi3DW mini-project/Assets/Scripts/EnemyHit.cs
+++ Pi3DW mini-project/Assets/Scripts/EnemyHit.cs
@@ -1,24 +1,15 @@
 using UnityEngine;
 
 public class EnemyHit : MonoBehaviour
-
 {
-    public GameObject lostText;
-    public GameObject missionText;
-    private bool hasLost = false;
+    public GameObject loseScreen;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enemy trigger hit by: " + other.name); 
-        if (hasLost) return;
-
         if (other.CompareTag("Player"))
         {
-            missionText.SetActive(false);
-            hasLost = true;
-            lostText.SetActive(true);
+            loseScreen.SetActive(true);
             Time.timeScale = 0f;
-            Debug.Log("Player caught! Lose screen shown.");
         }
     }
 }
